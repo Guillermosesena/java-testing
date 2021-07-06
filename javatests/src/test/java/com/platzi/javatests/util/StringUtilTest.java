edb1f6f99d3656e15c60c29d7fa0e852c39428ad
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-06 12:07:57 -0700

    5/24 Creating an unit test: error exception

--- javatests/src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ javatests/src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -2,14 +2,15 @@ package com.platzi.javatests.util;
 
 public class StringUtilTest {
     public static void main(String[] args) {
-        String result = StringUtil.repeat("Hi", 3);
-        if(!result.equals("HiHiHi")) {
-            System.out.println("ERROR");
-        }
 
-        String result2 = StringUtil.repeat("Hi", 1);
-        if(!result2.equals("Hi")) {
-            System.out.println("ERROR");
+        assertEquals(StringUtil.repeat("Hi", 3), "HiHiHi");
+
+        assertEquals(StringUtil.repeat("Hi", 1), "Hi");
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)) {
+            throw new RuntimeException(actual + " is not equal to expected " + expected);
         }
     }
 }
