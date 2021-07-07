bd058289c91f7131f86a5f4dc3795cb91a2e6b2c
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-06 18:06:01 -0700

    15/24 Challenge 2

---
+++ javatests/src/main/java/com/platzi/javatests/challenge2/FizzBuzz.java
@@ -0,0 +1,14 @@
+package com.platzi.javatests.challenge2;
+
+public class FizzBuzz {
+
+    public static String fizzBuzz(int n){
+        if( n % 3 == 0 && n % 5 == 0)
+            return "FizzBuzz";
+        else if(n % 3 == 0)
+            return "Fizz";
+        else if(n % 5 == 0)
+            return "Buzz";
+        return String.valueOf(n);
+    }
+}
