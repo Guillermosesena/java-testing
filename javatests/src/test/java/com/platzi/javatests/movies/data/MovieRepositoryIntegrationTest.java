697910620d83ea711526fa044e0944c5dff601bd
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-06 21:05:13 -0700

    20/24 Integration test: Update db

--- javatests/src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
+++ javatests/src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.platzi.javatests.movies.data;
 import com.platzi.javatests.movies.model.Genre;
 import com.platzi.javatests.movies.model.Movie;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,6 +11,7 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.Arrays;
 
@@ -16,18 +19,25 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class MovieRepositoryIntegrationTest {
-    @Test
-    public void load_all_movies() throws SQLException {
-        //db in memory
-        DataSource dataSource =
+    private MovieRepositoryJdbc movieRepository;
+    private DataSource dataSource;
+
+    @Before
+    public void setUp() throws Exception{
+        dataSource =
                 new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
 
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
-        MovieRepositoryJdbc movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+        movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+    }
 
+
+    @Test
+    public void load_all_movies() throws SQLException {
+        //db in memory
 
         Collection<Movie> movies = movieRepository.findAll();
 
@@ -37,5 +47,34 @@ public class MovieRepositoryIntegrationTest {
                 new Movie(3, "Matrix", 136, Genre.ACTION)
         )));
     }
+
+    @Test
+    public void load_by_id() throws SQLException {
+        //db in memory
+
+        Movie movie = movieRepository.findById(2);
+
+        assertThat(movie, is(new Movie(2, "Memento", 113, Genre.THRILLER)));
+
+    }
+
+    @Test
+    public void insert_a_movie() {
+        Movie movie = new Movie("Super 8",112,Genre.THRILLER);
+        movieRepository.saveOrUpdate(movie);
+
+        Movie movieFromDb = movieRepository.findById(4);
+
+        assertThat(movieFromDb, is(new Movie(4,"Super 8",112,Genre.THRILLER)));
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception{
+        final Statement s = dataSource.getConnection().createStatement();
+        s.execute("drop all objects delete files");
+    }
+
 
 }
