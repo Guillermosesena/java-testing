f7a88a4425403ad716bcd4090428c5dbe8c69169
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-06 18:58:06 -0700

    18/24 Movies app: Find by duration

--- javatests/src/test/java/com/platzi/javatests/movies/service/MovieServiceShould.java
+++ javatests/src/test/java/com/platzi/javatests/movies/service/MovieServiceShould.java
@@ -4,6 +4,7 @@ import com.platzi.javatests.movies.data.MovieRepository;
 import com.platzi.javatests.movies.model.Genre;
 import com.platzi.javatests.movies.model.Movie;
 import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -17,8 +18,11 @@ import java.util.stream.Collectors;
 import static org.junit.Assert.*;
 
 public class MovieServiceShould {
-    @Test
-    public void return_movies_by_genre() {
+
+    private MovieService movieService;
+
+    @Before
+    public void setUp() throws Exception {
         MovieRepository movieRepository = Mockito.mock(MovieRepository.class);
         Mockito.when(movieRepository.findAll()).thenReturn(
                 Arrays.asList(
@@ -31,10 +35,27 @@ public class MovieServiceShould {
                         new Movie(7, "Matrix", 136, Genre.ACTION)
                 )
         );
-        MovieService movieService = new MovieService(movieRepository);
-        Collection<Movie> movies = movieService.findMoviesByGenre(Genre.COMEDY);
-        List<Integer> movieIds = movies.stream().map(movie -> movie.getId()).collect(Collectors.toList());
-
-        assertThat(movieIds, CoreMatchers.is(Arrays.asList(3, 6)));
+        movieService = new MovieService(movieRepository);
     }
+
+    @Test
+    public void return_movies_by_genre() {
+        Collection<Movie> movies = movieService.findMoviesByGenre(Genre.COMEDY);
+
+        assertThat(getMovieIds(movies), CoreMatchers.is(Arrays.asList(3, 6)));
+    }
+    @Test
+    public void return_movies_by_legth() {
+
+        Collection<Movie> movies = movieService.findMoviesByLength(119);
+
+        assertThat(getMovieIds(movies), CoreMatchers.is(Arrays.asList(2, 3, 4, 5, 6)));
+
+    }
+
+    private List<Integer> getMovieIds(Collection<Movie> movies) {
+        List<Integer> movieIds = movies.stream().map(movie -> movie.getId()).collect(Collectors.toList());
+        return movieIds;
+    }
+
 }
